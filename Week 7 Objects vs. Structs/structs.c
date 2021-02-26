c2879ec54ad6172b719d8ebd35b58a190f0a5150
blcksm1th1992 <blcksm1th1992@gmail.com>
2021-02-26 01:26:59 -0600

    j

--- Week 7 Objects vs. Structs/structs.c
+++ Week 7 Objects vs. Structs/structs.c
@@ -3,27 +3,51 @@
 #include <string.h>
 #include <time.h>
 
-struct Food
+struct Powerlifter
 {
-	char name[20];
-	int calories;
+	int squat;
+    int deadlift;
+    int bench_press;
 
 }; 
 
+void kiloToPounds(struct Powerlifter Eddie, int s, int d, int b)
+{
 
+    Eddie.squat = Eddie.squat * 2.20462;
+    Eddie.deadlift = Eddie.deadlift * 2.20462;
+    Eddie.bench_press = Eddie.bench_press * 2.20462;
 
+    printf("Eddie has a %d pound Squat, %d pound Deadlift, %d pound Bench_Press\n\n", Eddie.squat, Eddie.deadlift, Eddie.bench_press);
+}
+
+void WSM(struct Powerlifter Eddie, int d)
+{
+    if (Eddie.deadlift > 516)
+    {
+        puts(" You have the world RECORD!!!" );
+    }
+    else if (Eddie.deadlift < 516 )
+    {
+        puts( "Better Luck Next time");
+    }
+    else 
+        puts("Error");
+} 
 
 int main()
 {
-    struct Food apple; 
+    srand(time(0));
+    struct Powerlifter Eddie; 
+    Eddie.squat = rand() % 600 + 1;
+    Eddie.deadlift = rand() % 600 + 1;
+    Eddie.bench_press = rand() % 600 + 1;
 
-    scanf("%s", apple.name);
+    printf("Eddie has a %d kilo Squat, %d kilo Deadlift, %d kilo Bench_Press\n\n", Eddie.squat, Eddie.deadlift, Eddie.bench_press);
 
-    scanf("%d", &apple.calories);
-    //printf("%s", apple.name); 
 
-    int dailyCal = dailyCal - apple.calories; 
+    WSM(Eddie, Eddie.deadlift);
+    kiloToPounds(Eddie, Eddie.squat, Eddie.deadlift, Eddie.bench_press);
 
-    printf("Calories left in day: %d\n", dailyCal);
     return 0; 
 }
